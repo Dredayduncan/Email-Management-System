ee4555f044b4391e2ae7d9a4a22bca165d2f84e7
Dredayduncan <dredayduncan@gmail.com>
2020-12-15 01:02:40 +0000

    PHP units tests

--- test.php
+++ test.php
@@ -1,10 +1,19 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+include_once("utility/control.php");
 
 final class test extends TestCase
 {
-    public function testFailure(): void
+    public function testSuccess(): void
     {
-        $this->assertContains(3, [1, 2, 3]);
+        // Check if it returns
+        $this->assertNotEmpty([newMessage()]);
+
+        // Check if the config File exists
+        $this->assertFileExists('verification/config.php');
+
+        // check if function is not empty
+        $this->assertNotNull([regularOptions()]);
     }
 }
+?>
